02298bba5042e38a1fd3d2497295ef514da3d7c4
zhaochengming <zhaochengming@meituan.com>
2019-10-21 23:32:50 +0800

    add new

---
+++ src/main/java/Chap01Fundamentals/Chap01_03/SingleLinkedList.java
@@ -0,0 +1,74 @@
+package Chap01Fundamentals.Chap01_03;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SingleLinkedList<Item>  {
+    private class Node {
+        Item item;
+        Node next;
+        public Node(Item v){
+            this.item = v;
+            this.next = null;
+        }
+    }
+
+    private Node first;
+    private int N;
+
+    public boolean isEmpty(){return this.first == null;}
+    public int size(){return this.N;}
+
+    public void addHead(Item item){
+        Node oldFirst = this.first;
+        this.first = new Node(item);
+        this.first.next = oldFirst;
+        this.N ++;
+    }
+
+    // Ex 19
+    public void deleteLastNode(){
+        if(this.N == 0) return;
+        if(this.N == 1)
+            this.first = null;
+        else
+            for(Node x = this.first; x != null; x = x.next){
+                if(x.next.next == null)
+                    x.next = null;
+            }
+        this.N --;
+    }
+
+    // Ex 20
+    public void delete(int k){
+        if(k > this.N) throw new IndexOutOfBoundsException("The K is out of List's bound.");
+        if(k == 1){
+            this.first = this.first.next;
+            return;
+        }
+        int index = 1;
+        for(Node x = this.first; x != null; x = x.next){
+            if(index == k - 1) {
+                x.next = x.next.next;
+                break;
+            }
+            index ++;
+        }
+    }
+
+    // Ex 21
+    public boolean find(Item key){
+        for(Node x = this.first; x != null; x = x.next){
+            if(x.item == key) return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        for(Node x = this.first; x != null; x = x.next)
+            res.append(x.item).append(" -> ");
+        return res.toString();
+    }
+}
