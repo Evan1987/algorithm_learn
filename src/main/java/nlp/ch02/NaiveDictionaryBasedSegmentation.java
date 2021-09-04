17142514776987a54ef55b1531cea43cdf770b2b
Evan <zcm1357@163.com>
2021-09-04 22:27:19 +0800

    add new

--- src/main/java/nlp/ch02/NaiveDictionaryBasedSegmentation.java
+++ src/main/java/nlp/ch02/NaiveDictionaryBasedSegmentation.java
@@ -2,7 +2,6 @@ package nlp.ch02;
 
 import com.hankcs.hanlp.corpus.io.IOUtil;
 import com.hankcs.hanlp.dictionary.CoreDictionary;
-
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
@@ -17,7 +16,7 @@ public class NaiveDictionaryBasedSegmentation {
     }
 
     public NaiveDictionaryBasedSegmentation() throws IOException {
-        this(IOUtil.loadDictionary("data/dictionary/CoreNatureDictionary.mini.txt"));
+        this(IOUtil.loadDictionary("F:/data/hanlp/dictionary/CoreNatureDictionary.mini.txt"));
     }
 
     public List<String> segmentFully(String text) {
@@ -30,6 +29,63 @@ public class NaiveDictionaryBasedSegmentation {
             }
         }
         return wordList;
+    }
+
+    public List<String> segmentForwardLongest(String text) {
+        List<String> wordList = new LinkedList<>();
+        for (int i = 0; i < text.length(); ) {
+            int z = i + 1;
+            for (int j = z; j <= text.length(); j ++) {
+                if (this.dictionary.containsKey(text.substring(i, j)))
+                    z = j;
+            }
+            wordList.add(text.substring(i, z));
+            i = z;
+        }
+        return wordList;
+    }
+
+    public List<String> segmentBackwardLongest(String text) {
+        List<String> wordList = new LinkedList<>();
+        for (int i = text.length(); i > 0; ) {
+            int z = i - 1;
+            for (int j = z; j >= 0; j --) {
+                if (this.dictionary.containsKey(text.substring(j, i)))
+                    z = j;
+            }
+            wordList.add(0, text.substring(z, i));
+            i = z;
+        }
+        return wordList;
+    }
+
+    private static int countSingleWord(List<String> wordList) {
+        int size = 0;
+        for (String word: wordList)
+            if (word.length() == 1)
+                size ++;
+        return size;
+    }
+
+    public List<String> segmentBidirectional(String text) {
+        List<String> forward = segmentForwardLongest(text);
+        List<String> backward = segmentBackwardLongest(text);
+
+        if (forward.size() < backward.size())
+            return forward;
+        else if (forward.size() > backward.size())
+            return backward;
+        if (countSingleWord(forward) < countSingleWord(backward))
+            return forward;
+        return backward;
+    }
+
+    public static void main(String[] args) throws IOException {
+        NaiveDictionaryBasedSegmentation seg = new NaiveDictionaryBasedSegmentation();
+        System.out.println(seg.segmentFully("就读北京大学"));
+        System.out.println(seg.segmentForwardLongest("就读北京大学"));
+        System.out.println(seg.segmentForwardLongest("研究生命起源"));
+        System.out.println(seg.segmentBackwardLongest("研究生命起源"));
     }
 
 
