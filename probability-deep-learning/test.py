2fd6dc3c6f6f1e498a3ebf32e711530efad48920
zhaochengming <zhaochengming@meituan.com>
2022-10-18 21:46:41 +0800

    update tfp-learning

--- probability-deep-learning/test.py
+++ probability-deep-learning/test.py
@@ -5,19 +5,14 @@
 
 import tensorflow as tf
 import tensorflow_probability as tfp
+from tensorflow_probability.python.layers import VariableLayer
 from tensorflow.keras.layers import Input, Dense
 from tensorflow.keras.models import Model
 from tensorflow_probability.python.distributions import distribution as tfd
 from tensorflow_probability.python.layers import DistributionLambda
 from tensorflow_probability.python.distributions import Normal
+from tensorflow.keras.constraints import Constraint
 
 
-def my_distr(params):
-    return tfp.distributions.Normal(loc=params, scale=0.01)
-
-
-inputs = Input(shape=(1,))
-distr = DistributionLambda(my_distr)(inputs)
-model = Model(inputs=inputs, outputs=distr)
 
 
