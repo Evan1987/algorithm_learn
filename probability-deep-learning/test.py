3c6317e8bc4a5b7cb0b798c7e0ff91e564f651aa
Evan <zcm1357@163.com>
2022-11-01 00:08:11 +0800

    update

--- probability-deep-learning/test.py
+++ probability-deep-learning/test.py
@@ -8,12 +8,16 @@ import tensorflow_probability as tfp
 from tensorflow_probability.python.layers import VariableLayer
 from tensorflow.keras.layers import Input, Dense
 from tensorflow.keras.models import Model
-import tensorflow_probability.python.distributions as tfd
+from tensorflow_probability.python.bijectors.real_nvp import RealNVP
+from tensorflow_probability.python.bijectors.masked_autoregressive import MaskedAutoregressiveFlow
 from tensorflow_probability.python.layers import DistributionLambda
 from tensorflow_probability.python.distributions import Normal
 from tensorflow.keras.constraints import Constraint
 
+tfb = tfp.bijectors
+tfd = tfp.distributions
 
-tfd.Mixture
 
-
+net = tfb.real_nvp_default_template([32, 32])
+b = RealNVP(shift_and_log_scale_fn=net, num_masked=2)
+print(b.trainable_variables)
