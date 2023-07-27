3ad81a18bf43a10614b2b372f07f798a5e5240d0
zhaochengming <zhaochengming@meituan.com>
2023-07-27 20:03:06 +0800

    update

--- mtl/mmoe/torch-mmoe/model.py
+++ mtl/mmoe/torch-mmoe/model.py
@@ -12,48 +12,74 @@ class MMoE(nn.Module):
     """Multi-gate Mixture-of-Experts for CT-CVR problem"""
 
     def __init__(self, *, user_feature_dict: Dict[str, Tuple], item_feature_dict: Dict[str, Tuple],
-                 embedding_length: int = 128, n_experts: int = 3, mmoe_hidden_size: int = 128,
-                 hidden_sizes: Tuple = (128, 64), output_size: int = 1, dropout: float = 0.5, n_tasks: int = 2):
+                 embedding_length: int = 128, n_experts: int = 3, n_tasks: int = 2, units: int = 128,
+                 use_expert_bias: bool = True, use_gate_bias: bool = True,
+                 tower_hidden_sizes: Tuple = (128, 64), output_size: int = 1, dropout: float = 0.5):
         """
         input parameters to build MMoE model
         :param user_feature_dict: {feature_name: (feature_unique_num, feature_index)}
         :param item_feature_dict: {feature_name: (feature_unique_num, feature_index)}
         :param embedding_length: the embedding vector's length
         :param n_experts: num of expert nets in MMoE
-        :param mmoe_hidden_size: mmoe layer input dimension
-        :param hidden_sizes: `tuple` for task tower hidden size
+        :param n_tasks: num of tasks
+        :param units: mmoe layer input dimension
+        :param use_expert_bias: Whether to use bias in expert weights
+        :param use_gate_bias: Whether to use bias in gate weights
+        :param tower_hidden_sizes: `tuple` for task tower hidden size
         :param output_size: task output size
         :param dropout: DNN dropout prob
-        :param n_tasks: num of tasks
         """
         super(MMoE, self).__init__()
         assert user_feature_dict and item_feature_dict, "Empty features"
-        assert n_tasks == len(hidden_sizes), "Unmatched num of tasks and task tower sizes"
 
         self.user_feature_dict = user_feature_dict
         self.item_feature_dict = item_feature_dict
         self.n_tasks = n_tasks
+        self.use_expert_bias = use_expert_bias
+        self.use_gate_bias = use_gate_bias
 
         # 共享embedding特征层
-        embed_count = 0
+        embed_feature_count = 0
+        side_feature_count = 0
         for feature, (feature_num, _) in self.user_feature_dict.items():
             if feature_num > 1:
-                embed_count += 1
+                embed_feature_count += 1
                 self.add_module(feature, nn.Embedding(feature_num, embedding_length))
+            else:
+                side_feature_count += 1
 
         for feature, (feature_num, _) in self.item_feature_dict.items():
             if feature_num > 1:
-                embed_count += 1
+                embed_feature_count += 1
                 self.add_module(feature, nn.Embedding(feature_num, embedding_length))
+            else:
+                side_feature_count += 1
 
-        total_size = embedding_length * embed_count + len(self.user_feature_dict) + len(self.item_feature_dict) - embed_count
+        feature_size = embedding_length * embed_feature_count + side_feature_count
 
-        # experts专家层 与experts数量有关
-        self.experts = nn.Parameter(torch.empty(total_size, mmoe_hidden_size, n_experts), requires_grad=True)
-        nn.init.normal_(self.experts, 0.0, 1.0)
-        self.experts_bias = nn.Parameter(torch.empty(total_size, n_experts), requires_grad=True)
-        nn.init.normal_(self.experts_bias, 0.0, 1.0)
+        # experts专家层 与experts数量有关 [F, H, N_E]
+        self.expert_kernels = nn.Parameter(torch.empty(feature_size, units, n_experts), requires_grad=True)
+        nn.init.normal_(self.expert_kernels, 0.0, 1.0)
 
-        # gates门限层 与 task数量有关
-        self.gates = nn.ModuleList([nn.Linear(total_size, n_experts, bias=True) for _ in range(n_tasks)])
+        self.expert_bias = None
+        if self.use_expert_bias:
+            # [H, N_E]
+            self.expert_bias = nn.Parameter(torch.empty(units, n_experts), requires_grad=True)
+            nn.init.normal_(self.experts_bias, 0.0, 1.0)
+
+        # gates门限层 与 task数量有关 [H, N_E] * N_T
+        self.gate_kernels = []
+        for _ in range(n_tasks):
+            gate_kernel = nn.Parameter(torch.empty(units, n_experts), requires_grad=True)
+            nn.init.normal_(gate_kernel, 0.0, 1.0)
+            self.gate_kernels.append(gate_kernel)
+
+        self.gate_bias = []
+        if self.use_gate_bias:
+            self.gate_bias = [nn.Parameter(torch.randn(n_experts), requires_grad=True) for _ in range(n_tasks)]
+
+        # esmm 中的 CTR CTCVR独立tower
+        for i in range(self.n_tasks):
+            self.add_module(f"", nn.ModuleList())
+
 
